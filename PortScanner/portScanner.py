2d13bfd13ab88a4ca9363e2ce69284ae3058e935
Ryan Lindner <ryan@Ryans-MacBook-Air.local>
2019-05-24 16:24:13 -0700

    fixed threading

--- PortScanner/portScanner.py
+++ PortScanner/portScanner.py
@@ -1,48 +1,39 @@
-import socket #using built in module to access sockets
 import threading
-import sys #for exiting invalid inputs
-import ping #user created file for pinging before scanning
-from datetime import datetime #for timing
+import socket
+import sys
+import time
+from Queue import Queue
 
-target = raw_input("Enter the DNS to target: ")
-check = ping.pingTarget(target)
-print(check)
-if check != "Success":
-    sys.exit()
-targetIP = socket.gethostbyname(target)
-limit = input("Up to what ports would you like to scan: ") #has to be greater than 1 and less than 65535
-if limit < 1 or limit > 65535:
-    print("Port number out of range. Exiting")
-    sys.exit()
+print_lock = threading.Lock()
 
-t1 = datetime.now()
-screenLock = Semapohre(value=1) #using a semaphore to allow for threading but locking screen prints
-for port in range(1, limit):
-    t = Thread(target = scan, args = (targetIP, port))
+if len(sys.argv) !=2 :
+    print ("Usage: portscan.py <host>")
+    sys.exit(1)
+
+host = sys.argv[1]
+
+def scan(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        con = s.connect((host, port))
+        with print_lock:
+            print('Port: ' + str(port) + ' is open')
+        con.close()
+    except:
+        pass
+
+def threader():
+    while True:
+        worker = q.get()
+        scan(worker)
+        q.task_done()
+
+q = Queue()
+
+for x in range(100):
+    t = threading.Thread(target=threader)
+    t.daemon = True
     t.start()
 
-def scan(targetIP, port):
-try:
-    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #creating a socket stream
-    result = mySocket.connect_ex((targetIP, port))
-    if result == 0:
-        screenLock.acquire()
-        print ("Port {}:      Open".format(port))
-
-except socket.gaierror:
-    print ("DNS could not be resolved. Exiting")
-    sys.exit()
-
-except socket.error:
-    print ("Failed connection to server")
-    sys.exit()
-
-finally:
-    screenLock.release()
-    mySocket.close()
-
-t2 = datetime.now()
-
-timeElapsed = t2 - t1
-
-print "\nScanning completed in: ", timeElapsed
+for worker in range(1, 1024):
+    q.put(worker)
